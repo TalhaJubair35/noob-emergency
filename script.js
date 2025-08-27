abd97654da5f57cd64779669590a944d825eb485
TalhaJubair35 <jubairtalha15@gmail.com>
2025-08-28 01:36:22 +0600

    all variables added

--- script.js
+++ script.js
@@ -7,3 +7,74 @@ for (const button of heartButtons) {
   });
 }
 
+let callButtons=document.getElementsByClassName('call-button');
+let numbers=document.getElementsByClassName('number');
+//////////////////////////////////
+let coin=document.getElementById('coin');
+let copy=document.getElementById('copy');
+
+// ///////////////////////////
+
+let nationalEmergencyTitle=document.getElementById('national-emergency-title');
+let nationalEmergencyNumber=document.getElementById('national-emergency-number');
+let nationalEmergencyCopyButton=document.getElementById('national-emergency-copy-button');
+let nationalEmergencyCallButton=document.getElementById('national-emergency-call-button');
+
+////////////////////////////////////
+
+let policeTitle=document.getElementById('police-title');
+let policeNumber=document.getElementById('police-number');
+let policeCopyButton=document.getElementById('police-copy-button');
+let policeCallButton=document.getElementById('police-call-button');
+
+///////////////////////////////
+
+let fireServiceTitle=document.getElementById('fire-service-title');
+let fireServiceNumber=document.getElementById('fire-service-number');
+let fireServiceCopyButton=document.getElementById('fire-service-copy-button');
+let fireServiceCallButton=document.getElementById('fire-service-call-button');
+
+///////////////////
+
+let ambulanceServiceTitle=document.getElementById('ambulance-service-title');
+let ambulanceServiceNumber=document.getElementById('ambulance-service-number');
+let ambulanceServicecopyButton=document.getElementById('ambulance-service-copy-button');
+let ambulanceServiceCallButton=document.getElementById('ambulance-service-call-button');
+
+//////////////////////
+
+let womenChildHelplineTitle=document.getElementById('women-child-helpline-title');
+let womenChildHelplineNumber=document.getElementById('women-child-helpline-number');
+let womenChildHelplineCopyButton=document.getElementById('women-child-helpline-copy-button');
+let womenChildHelplineCallButton=document.getElementById('women-child-helpline-call-button');
+
+////////////
+
+let antiCorruptionHelplineTitle=document.getElementById('anti-corruption-helpline-title');
+let antiCorruptionHelplineNumber=document.getElementById('anti-corruption-helpline-number');
+let antiCorruptionHelplineCopyButton=document.getElementById('anti-corruption-helpline-copy-button');
+let antiCorruptionHelplineCallButton=document.getElementById('anti-corruption-helpline-call-button');
+
+///////////
+
+let electricityHelplineTitle=document.getElementById('electricity-helpline-title');
+let electricityHelplineNumber=document.getElementById('electricity-helpline-number');
+let electricityHelplineCopyButton=document.getElementById('electricity-helpline-copy-button');
+let electricityHelplineCallButton=document.getElementById('electricity-helpline-call-button');
+
+///////////
+
+let bracHelplineTitle=document.getElementById('brac-helpline-title');
+let bracHelplineNumber=document.getElementById('brac-helpline-number');
+let bracHelplineCopyButton=document.getElementById('brac-helpline-copy-button');
+let bracHelplineCallButton=document.getElementById('brac-helpline-call-button');
+
+////////////
+
+let bangladeshRailwayHelplineTite=document.getElementById('bangladesh-railway-helpline-tite');
+let bangladeshRailwayHelplineNumber=document.getElementById('bangladesh-railway-helpline-number');
+let bangladeshRailwayHelplineCopyButton=document.getElementById('bangladesh-railway-helpline-copy-button');
+let bangladeshRailwayHelplineCallButton=document.getElementById('bangladesh-railway-helpline-call-button');
+
+////////////
+
